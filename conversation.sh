e3f4e991c0c22bc6bd86e7ce3095b9bcf8e5fc6e
naresh <nareshveeranala@gmail.com>
2025-07-10 12:36:38 +0530

    hello worold started

--- conversation.sh
+++ conversation.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-NUM1=100
-NUM2=100
+movies=("court" "hit 3" "pushpha")
 
-
-timestamp=$(date)
-
-echo "script executed at : $timestamp"
-
-SUM=$(($NUM1+$NUM2))
-echo "sum of $Num1 and $Num2 is: $SUM"
+ech0 "first movie: ${movies[0]}"
