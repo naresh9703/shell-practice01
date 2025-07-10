6e3f6effbf02cf5d004e865ef47091397b5c5012
naresh <nareshveeranala@gmail.com>
2025-07-10 11:57:18 +0530

    hello worold started

---
+++ conversation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=modi
+PERSON2=musk
+
+echo "$PERSON1":: hey $PERSON2, how are you?"
+echo "$PERSON2":: hello $PERSON1, I am fine, what about you"
+echo "$PERSON1":: i am fine, what's up?"
+echo "$PERSON2":: Nothing, just going to mars now, will you come?"
+echo "$PERSON1":: sorry , you can carry on! i wll come next time"
+
