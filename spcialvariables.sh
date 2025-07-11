db691b99afb5ba5c0f2f2c387d4e0e81abafced8
naresh <nareshveeranala@gmail.com>
2025-07-11 12:24:19 +0530

    conditions created

--- spcialvariables.sh
+++ spcialvariables.sh
@@ -5,17 +5,25 @@ userid=$(id -u)
 if [ userid -ne 0 ]
 then
     echo "Error:please run the script with root access.."
-    #exit 1
+    exit 1
 else
     echo "your running with root access"
 fi
 
-dnf install mysqlklkzdsl -y
+dnf list installed mysql
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
+then
+    echo "My sql is not installed...going to install it..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
 then
     echo "installing mysql is success...."
 else
     echo "installing mysql is failure....."
     exit 1
 fi
+else
+    echo "My sql is already installed...nothing to do..."
+fi
+
