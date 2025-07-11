f2dcaf01ba2508ee597ef78f5f57aea1d9b03ec3
naresh <nareshveeranala@gmail.com>
2025-07-11 11:56:28 +0530

    conditions created

--- spcialvariables.sh
+++ spcialvariables.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-  
-NUMBER=$1
+userid=$(id -u)
 
-if [ $NUMBER -lt 10 ] 
+if [ userid -ne 0 ]
 then
-    echo "Given number $NUMBER is less than 10"
+    echo "Error:please run the script with root access.."
+    exit 1
 else
-    echo "Given number $NUMBER is not less than 10"
+    echo "ypur running with root access"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "installing mysql is success...."
+else
+    echo "installing mysql is failure....."
+    exit 1
 fi
