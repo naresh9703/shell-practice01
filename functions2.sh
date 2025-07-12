4dd8b62f576aeaadec317e2dd4615fe15d43af6d
naresh <nareshveeranala@gmail.com>
2025-07-12 16:20:03 +0530

    creating functions

--- functions2.sh
+++ functions2.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 userid=$(id -u)
-LOGS_FOLDER="/var/logs/shell/shell-practice01-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
