cdbdf4d195b15ec0782aa3f218178febcbaa4b55
naresh <nareshveeranala@gmail.com>
2025-07-15 19:39:47 +0530

    creating variables

--- practice.sh
+++ practice.sh
@@ -5,7 +5,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "user is not running with root access, please check the RC are re run"
-    exit 1
+    #exit 1
 else
     echo "user is running with root access"
 fi
