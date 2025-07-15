88f4c1b2fc38bf75ee7f4719f196b2785c3a8b75
naresh <nareshveeranala@gmail.com>
2025-07-15 18:54:32 +0530

    creating variables

---
+++ practice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Value1=$1
+Value2=$2
+Value3=$3
+
+
+echo "Hello $2 bro How are you?"
+echo "Hello $1 bro i am good , how about you?"
+echo "Yeah! i am good"
+echo "Hello $3 bro How are you?"
+echo "Yeah! i am good"
+
