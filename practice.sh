f3e3d70c98b0dfd28524f5e1ced87e2a69f553e0
naresh <nareshveeranala@gmail.com>
2025-07-15 19:24:37 +0530

    creating variables

--- practice.sh
+++ practice.sh
@@ -9,7 +9,7 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]
 then 
     echo "Given number $Number is greater then 20 "
-else
+elif
     echo "Given number $Number is less then 20"
 else 
    echo "Given number $Number is not equal to 20"
