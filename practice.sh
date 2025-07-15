6a12ca60c524148d756b2fba658dc97d29fd1838
naresh <nareshveeranala@gmail.com>
2025-07-15 19:10:22 +0530

    creating variables

--- practice.sh
+++ practice.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-MOVIES= $1
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+
+
+
 
 echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
 echo "First Movie: ${MOVIES[4]}"
 
 echo "All movies: ${MOVIES[@]}"
+
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
