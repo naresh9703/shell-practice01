5485b6b3171f21cf0a47b2a65c7928cac2e1c7ef
naresh <nareshveeranala@gmail.com>
2025-07-25 22:26:37 +0530

    deleting logs

--- diskusage.sh
+++ diskusage.sh
@@ -9,5 +9,10 @@ while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}'| cut -d "%" -f1)
     partition=$(echo $line | awk '{print $7F}')
-    echo "$partition: $USAGE"
+    if [ $USAGE -ge $disk_threshold ]
+    then
+    MSG+="High Disk Usage on $partition: $USAGE \n"
+    fi
 done <<< $disk_usage
+
+echo -e $MSG
